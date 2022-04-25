81580e42f7bd64a305aab017f02c50c7c5b1b3be
a_szczepanik <a.szczepanik@adform.com>
2022-04-25 12:57:31 +0200

    Add implementation of leave method and tests

--- src/main/scala/Room.scala
+++ src/main/scala/Room.scala
@@ -1,17 +1,30 @@
-case class Room(name: String, capacity: Int = 10) {
+case class People(people: Int)
+
+case class Room(name: String, capacity: Int) {
 
   var rooms = 0
 
-  def enter(people: Int): Boolean = {
-    if (capacity + people >= rooms && people <= capacity) {
-      rooms += people
+  def enter(people: People): Boolean = {
+    if (people.people <= capacity) {
+      rooms += people.people
       true
     }
     else false
   }
 
-  def leave(people: Int, roomName: String): Boolean = {
-    true
+  def leave(people: People, room: Room): Boolean = {
+    if (people.people > capacity) {
+      rooms -= people.people
+      false
+    }
+    else true
   }
 
+  def printStatus: String = s"Status of people in room $name: $capacity"
+
+}
+
+object Main extends App {
+  val room1 = Room("1", 5)
+  val room2 = Room("10", 10)
 }
--- src/test/scala/RoomSpec.scala
+++ src/test/scala/RoomSpec.scala
@@ -6,14 +6,28 @@ class RoomSpec extends AnyFlatSpec with should.Matchers {
 
   behavior of "Room"
 
-  val room: Room = Room("Room 123")
+  val room: Room = Room("123", 10)
+  val anotherRoom: Room = Room("321", 0)
 
   "room.enter" should "return true if given amount of people can enter the room" in {
-    room.enter(9) shouldBe true
+    room.enter(People(9)) shouldBe true
   }
 
   it should "return false if people cannot enter the room" in {
-    room.enter(11) shouldBe false
+    room.enter(People(11)) shouldBe false
+  }
+
+  "room.leave" should "return true if person left the room" in {
+    room.leave(People(2), room) shouldBe true
+  }
+
+  it should "return false when given amount of people is over the capacity" in {
+    room.leave(People(11), room) shouldBe false
+  }
+
+  "room.printStatus" should "report the number of people inside of the room" in {
+    room.printStatus shouldBe "Status of people in room 123: 10"
+    anotherRoom.printStatus shouldBe "Status of people in room 321: 0"
   }
 
 }
